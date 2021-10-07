bbc5367bf5e3ee127b4fd6f097eae7d45b850d66
JOEL0316 <90368526+JOEL0316@users.noreply.github.com>
2021-10-07 13:22:24 -0500

    Inecuaciones.m
    Resolucion de inecuaciones de primer grado con una incognita,segundo grado con una incognita  y de valor absoluto.

---
+++ Inecuaciones.m
@@ -0,0 +1,39 @@
+%Octave Script
+%Title             :Resolucion de desigualdades
+%Description       :Script para resolver inecuaciones(primer grado,segundo grado,valor absoluto)
+%Author            :Joel Emmanuel De La Cruz Rodriguez,Maria Guadalupe Martinez Narvaes,Osvaldo Hernandez Martinez
+%Date              :20211007
+%Version           :1
+%Usage             :C:\Users\Leo\
+%Notes             :Requiere aplicacion obtave
+
+
+
+%Inecuaciones de primer grado con una incognita
+syms x
+disp ( 'Inecuaciones de primer grado' );
+disp (6*x+9<=5+x-2);
+solve(6*x+9<=5*x-2)
+
+
+
+%Inecuaciones de segundo grado con una incognita
+syms x
+disp ( 'Inecuaciones de segundo grado' );
+disp (x^-6*x+8>=0);
+solve(x^-6*x+8>=0)
+
+
+%Inecuancion con valor absoluto
+syms x
+disp ( 'Inecuacion con valor absoluto' );
+disp ( 'abs (x^2-3*x)>4');
+solve (abs (x^2-3*x)>7')
+
+x=-25:0.06:14;
+y=x-10;
+subplot(2,3,2);
+plot(x,y)
+xlabel("x");
+ylabel("-oo");
+title("La inecuacion es: (x^2-3*x)>4");
